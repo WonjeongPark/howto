d451e8884c2372ca588bcabc0efaccda85ea94d5
WonjeongPark <bnhs1127@gmail.com>
2019-03-31 23:22:39 +0900

    Main- form, input ing

--- src/components/TrainerProfileFrom/TrainerProfileForm.jsx
+++ src/components/TrainerProfileFrom/TrainerProfileForm.jsx
@@ -3,12 +3,33 @@ import React, { Component } from 'react'
 
 
 class TrainerProfileForm extends Component {
+  state = {
+    name: '',
+    phone: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
   render() {
     return (
-      <div>
-        <h1>Hi</h1>
-      </div>
-    )
+      <form>
+        <input
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+        />
+        <input
+          placeholder="전화번호"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          name="phone"
+        />
+        <div>{this.state.name} {this.state.phone}</div>
+      </form>
+    );
   }
 }
 
