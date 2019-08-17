371f264b622230f81b7d59db8505137b35bc8678
WonjeongPark <bnhs1127@gmail.com>
2019-08-17 16:28:08 +0900

    after merge, master branch setting as featureJ.

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -45,82 +45,42 @@ const models = require('../../models/models');
       }
     }).then(() => res.status(204).send());
   };
-  
-  // exports.create = (req, res) => {
-  //   const name = req.body.name
-  //   const gym = req.body.gym
-  //   const gender = req.body.gender
-  //   const career = req.body.career
-  //   const dates = req.body.dates
-  //   const bodypart = req.body.bodypart
-  //   const playerSource = req.body.playerSource
-  //   const count = req.body.count
-  //   const Num = req.body.Num
 
-  //   models.User.create({
-  //     name : name,
-  //     gym : gym,
-  //     gender : gender,
-  //     career : career,
-  //     bodypart : bodypart,
-  //     playerSource : playerSource,
-  //     count : count,
-  //     Num : Num,
-  //     // dates : [
-  //     //   dates
-  //     // ]
-  //     // }, { 
-  //     //   include : [{
-  //     //     dates
-  //     //     }]
-  //     })
-  //     .then(User => User.addDates( dates ))
-  //     .then((user) => {res.status(201).json(user)
-  //       console.log("데이터 추가 성공");
-  //       // res.redirect("/users")
-  //   })
-  //     .catch( err => {
-  //       console.log("데이터 추가 실패");
-  //     }) 
-  //   // models.dates.Create({
-  //   //   dates : {dates : dates}
-  //   // })
-  // };
   //TEST 만들고 옮기기
-  // exports.create = (req, res) => {
-  //     const name = req.body.name
-  //     const gym = req.body.gym
-  //     const gender = req.body.gender
-  //     const career = req.body.career
-  //     const dates = req.body.dates
-  //     const bodypart = req.body.bodypart
-  //     const playerSource = req.body.playerSource
-  //     const count = req.body.count
-  //     const Num = req.body.Num
-  //   return Promise.all([
-  //     models.User.create({
-  //           name : name,
-  //           gym : gym,
-  //           gender : gender,
-  //           career : career,
-  //           bodypart : bodypart,
-  //           playerSource : playerSource,
-  //           count : count,
-  //           Num : Num
-  //         })
-  //   ]),
-  //     models.dates.create({
-  //        dates : dates
+  exports.create = (req, res) => {
+      const name = req.body.name
+      const gym = req.body.gym
+      const gender = req.body.gender
+      const career = req.body.career
+      const dates = req.body.dates
+      const bodypart = req.body.bodypart
+      const playerSource = req.body.playerSource
+      const count = req.body.count
+      const Num = req.body.Num
+    return Promise.all([
+      models.User.create({
+            name : name,
+            gym : gym,
+            gender : gender,
+            career : career,
+            bodypart : bodypart,
+            playerSource : playerSource,
+            count : count,
+            Num : Num
+          })
+    ]),
+      models.dates.create({
+         dates : dates
          
-  //     })
-  //     .then(
-  //       ([id, dates])=> {
-  //         return Promise.all([
-  //           dates.setUser(id)
-  //         ])
-  //       }
-  //     ).catch(error => console.log(error));
-  //   }
+      })
+      .then(
+        ([id, dates])=> {
+          return Promise.all([
+            dates.setUser(id)
+          ])
+        }
+      ).catch(error => console.log(error));
+    }
 
   exports.update = (req, res) => {
     res.send()
