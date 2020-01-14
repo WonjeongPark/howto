ecd3bac375fc17f9327d6b430f1394a72c9d43a6
WonjeongPark <bnhs1127@gmail.com>
2020-01-15 00:35:53 +0900

    signin->data ok, server post /login 안넘어감

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
+var cookieParser= require('cookie-parser');
+var session = require('express-session')
+var passport = require('passport')
+const models = require('./models/models');
+const port = process.env.PORT || 5000;
+
 app.use( bodyParser.urlencoded({ extended: false }) );
 app.use( bodyParser.json() );
-
-const port = process.env.PORT || 5000;
 app.use(express.json())
-
-// console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
-
-const models = require('./models/models');
+// app.get('/express_backend', (req, res) => {
+//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// });
 models.sequelize.sync()
   .then(() => {
     console.log(' DB connection success. Press CTRL-C to stop\n');
@@ -26,8 +25,55 @@ models.sequelize.sync()
     process.exit();
   });
 
+app.use(session({
+  secret: 'howto secret key',
+  resave: false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.unsubscribe(passport.session());
 
+passport.serializeUser(function(id, done){
+  connection.query('SELECT * FROM users WHERE `id`=?',[id], function(err, rows){
+    var user = rows[0];
+    done(err,user)
+  });
+});
+
+passport.use(new LocalStrategy({
+  usernameField : 'loginID',
+  passwordField : 'loginPW'
+}, function(loginID, loginPW, done ){
+  connection.query('SELECT * FROM users WHERE `loginID`=?', [loginID], function(err, rows){
+    var user = rows[0];
+    if(err) {
+      return done(err)
+    }
+    if(!user){
+      return done(null, false, {message: 'Incorrect loginID'});
+    }
+    if(user.loginPW != loginPW){
+      return done(null, fasle, {message: 'Incorrect loginPW'})
+    }
+    return done(null, user)
+  });
+}));
 
 app.use('/users', require('./api/users/index'));
 // app.use('/login', require('./api/users/index'));
+
+// app.route('/login').get(function(req, res, next){
+//   console.log(req);
+//   if(req.user) {
+//     res.send('already login');
+//   } else {
+//     res.sendFile(__dirname+ './client/src/components/SignInForm/SignIn.jsx');
+//   }
+// }).post(passport.authenticate('local', {
+//   successRedirect:'/TrainerList',
+//   failureRedirect: '/login',
+//   failureFlash: true
+// }))
+app.post('/login', function(){console.log('post test')})
+
 module.exports = app;
